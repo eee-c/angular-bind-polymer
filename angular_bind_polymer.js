2d48e10776b67d21695b7c043ddd280367f0da58
Chris Strom <github@eeecomputes.com>
2015-08-09 14:04:30 -0400

    Fix (suspected) observers assignment issue

--- angular_bind_polymer.js
+++ angular_bind_polymer.js
@@ -4,43 +4,53 @@ directive('bindPolymer', ['$parse', function($parse) {
   return {
     restrict: 'A',
     scope : false,
-    compile: function bindPolymerCompile($element, $attr) {
+    compile: function bindPolymerCompile($element, $attrs) {
       var attrMap = {};
 
-      for (var prop in $attr) {
-        if (angular.isString($attr[prop])) {
-          var _match = $attr[prop].match(/\{\{\s*([\.\w]+)\s*\}\}/);
+      var el = $element[0];
+      for (var prop in $attrs) {
+        var attributeValue = el._config ? el._config[prop] : $attrs[prop];
+        if (angular.isString(attributeValue)) {
+          var _match = attributeValue.match(/\{\{\s*([\.\w]+)\s*\}\}/);
           if (_match) {
             attrMap[prop] = $parse(_match[1]);
           }
         }
       }
 
-      return function bindPolymerLink(scope, element, attrs) {
-
+      function bindPolymerLink(scope, element) {
         // When Polymer sees a change to the bound variable,
         // $apply / $digest the changes here in Angular
         var observer = new MutationObserver(function processMutations(mutations) {
           mutations.forEach(function processMutation(mutation) {
-            var attributeName, newValue, oldValue, getter;
-            attributeName = mutation.attributeName;
-
-            if(attributeName in attrMap) {
-              newValue = element.attr(attributeName);
-              getter = attrMap[attributeName];
-              oldValue = getter(scope);
-
-              if(oldValue != newValue && angular.isFunction(getter.assign)) {
-                getter.assign(scope, newValue);
-              }
-            }
+            _assignNgGetterFromPolymer(mutation.attributeName, scope, element);
           });
           scope.$apply();
         });
 
         observer.observe(element[0], {attributes: true});
         scope.$on('$destroy', observer.disconnect.bind(observer));
+        _assignInitialGetters(scope, element);
+      };
+
+      function _assignNgGetterFromPolymer(attributeName, scope, element) {
+        if (!(attributeName in attrMap)) return;
+        if (!element[0].$) return;
+
+        var newValue = element.attr(attributeName),
+            getter = attrMap[attributeName];
+
+        if (!angular.isFunction(getter.assign)) return;
+
+        // console.log(getter(scope) + ' -> ' + newValue)
+        getter.assign(scope, newValue);
       }
+
+      function _assignInitialGetters(scope, element) {
+        for (var prop in attrMap) { _assignNgGetterFromPolymer(prop, scope, element); }
+      }
+
+      return bindPolymerLink;
     }
   };
 }]);
